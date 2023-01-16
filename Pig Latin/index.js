59f47d81c69dd39e39c7b5b69b84ea0e38662924
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-17 02:03:32 +0300

    Solution for the Algo

--- Pig Latin/index.js
+++ Pig Latin/index.js
@@ -1,5 +1,22 @@
 function translatePigLatin(str) {
-    return str;
+    let vowels = ["a", "e", "i", "o", "u"];
+    let consonantHolder = []
+    let remainderHolder = []
+    for(let i = 0; i < str.length; i++){
+        if(vowels.indexOf(str[i])==-1){
+            consonantHolder.push(str[i])
+        } else {
+            remainderHolder.push(str.slice(i));
+            break;
+        }
+    }
+
+    if(consonantHolder.length != 0){
+        return `${remainderHolder.concat(consonantHolder.join("")).join("")}ay`;
+    } else {
+        return `${str}way`
+    }
+    
   }
   
-  translatePigLatin("consonant");
+  console.log(translatePigLatin("algorithm"));
