948c24f5aeae157b278abbb58fc1455852f671cc
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-17 04:38:18 +0300

    Added Missing Letters Test

---
+++ Missing Letters/index.js
@@ -0,0 +1,19 @@
+function fearNotLetter(str) {
+    let letterString = "abcdefghijklmnopqrstuvwxyz"
+    
+    let lowerStrCharPosition = letterString.indexOf(str[0])
+    let upperStrCharPosition = letterString.indexOf(str[str.length -1])
+    let strLen = upperStrCharPosition - lowerStrCharPosition+1
+    let fullRangeArr = []
+    for(let i = 0; i < strLen; i++){
+        fullRangeArr.push(letterString[lowerStrCharPosition+i])
+    }
+
+    let fullRangeArrLength = fullRangeArr.length;
+    for(let i = 0; i < fullRangeArrLength; i++){
+        if()
+    }
+    return fullRangeArr;
+  }
+  
+  console.log(fearNotLetter("bceh"));
