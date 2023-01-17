3b11306499bfae3af930e57b8b581735792471b5
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-17 03:53:24 +0300

    Cleared with the solution

--- DNA Pairing/index.js
+++ DNA Pairing/index.js
@@ -1,5 +1,17 @@
 function pairElement(str) {
-    return str;
+    let resultArr = [];
+    for(let i = 0; i < str.length; i++){
+        if(str[i] === "A"){
+            resultArr.push([str[i], "T"])
+        } else if(str[i] === "T"){
+            resultArr.push([str[i], "A"])
+        } else if(str[i] === "G"){
+            resultArr.push([str[i], "C"])
+        } else if(str[i] === "C"){
+            resultArr.push([str[i], "G"])
+        }
+    }
+    return resultArr;
   }
   
-  pairElement("GCG");
+  console.log(pairElement("ATCGA"));
