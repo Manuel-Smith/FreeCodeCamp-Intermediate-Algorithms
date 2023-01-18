5fe27b3cb3fc6bee8da84f3f00a9709290e55392
Manuel-Smith <riddicksmith37@gmail.com>
2023-01-18 11:20:13 +0300

    Added solution

--- Sum All Odd Fibonacci Numbers/index.js
+++ Sum All Odd Fibonacci Numbers/index.js
@@ -1,5 +1,21 @@
 function sumFibs(num) {
-    return num;
+  let newArr = [];
+  let oddCount = 0;
+  for(let i = 0; i <= num; i++){
+    if(i == 1 || i == 2){
+      newArr.push(1)
+    }
+    else if(i > 2){
+      newArr.push(newArr[newArr.length - 1]+newArr[newArr.length - 2])
+    }
   }
-  
-  sumFibs(4);
+
+  for(let i = 0; i < newArr.length; i++){
+    if(newArr[i]%2 == 1 && newArr[i] <= num){
+      oddCount +=newArr[i];
+    }
+  }
+  return oddCount;
+}
+
+console.log(sumFibs(75025))
