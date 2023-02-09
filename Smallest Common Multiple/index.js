0e9bf83af0996a1aa8ff03a883078c1f40337b5e
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-10 01:36:50 +0300

    Added solution for smallest common multiple, but not sure of it's complexity

--- Smallest Common Multiple/index.js
+++ Smallest Common Multiple/index.js
@@ -1,6 +1,38 @@
-function smallestCommons(arr){
+function smallestCommons(arr) {
+  let minArr = Math.min(...arr);
+  let maxArr = Math.max(...arr);
 
-    return arr;
+  let elementsArr = [];
+  let elementsArrLength = maxArr - minArr + 1;
+  let possibleMultiples = [];
+
+  for(let i = 0; i < elementsArrLength; i++){
+    elementsArr.push(minArr + i);
+  }
+
+  let limitedFactorial = 1;
+
+  for(let i = 0; i < elementsArrLength; i++){
+    limitedFactorial *= elementsArr[i];
+  }
+
+  function divisibilityTest(num){
+    for(let i = 0; i < elementsArrLength; i++){
+      if(num % elementsArr[i] != 0){
+        return false;
+      }
+    }
+    return true;
+  }
+
+  for(let i = 1; i <= limitedFactorial; i++){
+    if(divisibilityTest(i)){
+      possibleMultiples.push(i);
+      break;
+    }
+  }
+
+  return possibleMultiples[0];
 }
 
-smallestCommons([1, 5])
+console.log(smallestCommons([23,18]));
