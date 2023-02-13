d01348bd8e0fefce1c1fef020f07b9c35213c16c
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-13 13:26:44 +0300

    Added solution for Mapp the Debris

--- Map the Debris/index.js
+++ Map the Debris/index.js
@@ -1,7 +1,27 @@
 function orbitalPeriod(arr) {
-    const GM = 398600.4418;
-    const earthRadius = 6367.4447;
-    return arr;
+  const GM = 398600.4418;
+  const earthRadius = 6367.4447;
+  const PI = Math.PI;
+
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let craftName = arr[i].name;
+    let avgAlt = arr[i].avgAlt;
+    let radius = earthRadius + avgAlt;
+
+    let periodT = Math.round(2 * PI * Math.sqrt(radius ** 3 / GM));
+
+    resultArr.push({ name: craftName, orbitalPeriod: periodT });
   }
-  
-  orbitalPeriod([{name : "sputnik", avgAlt : 35873.5553}]);
+
+  return resultArr;
+}
+
+console.log(
+  orbitalPeriod([
+    { name: "iss", avgAlt: 413.6 },
+    { name: "hubble", avgAlt: 556.7 },
+    { name: "moon", avgAlt: 378632.553 },
+  ])
+);
