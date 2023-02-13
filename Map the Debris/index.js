399d8f2e455885c83ce3cc618f395189b64b89ac
Manuel-Smith <riddicksmith37@gmail.com>
2023-02-13 13:32:08 +0300

    Made a pure function out of it

--- Map the Debris/index.js
+++ Map the Debris/index.js
@@ -1,13 +1,14 @@
 function orbitalPeriod(arr) {
+  let workArr = [...arr];
   const GM = 398600.4418;
   const earthRadius = 6367.4447;
   const PI = Math.PI;
 
   let resultArr = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    let craftName = arr[i].name;
-    let avgAlt = arr[i].avgAlt;
+  for (let i = 0; i < workArr.length; i++) {
+    let craftName = workArr[i].name;
+    let avgAlt = workArr[i].avgAlt;
     let radius = earthRadius + avgAlt;
 
     let periodT = Math.round(2 * PI * Math.sqrt(radius ** 3 / GM));
